cadcaeb83daa8e5cd0433faf8f1c1379fbd9ad21
CloudMojos <joshuel.simbulan@gmail.com>
2023-04-18 11:50:27 +0800

    before using webpack

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-var map = L.map('map').setView([14.8143, 121.0725], 13);
+var map = L.map('map').setView([14.590666, 120.985197], 18);
+var osmtogeojson = require('osmtogeojson');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var bbox = map.getBounds().toBBoxString();
+
+bbox = bbox.split(',')
+let validBbox = bbox[1] + ',' + bbox[0] + ',' + bbox[3] + ',' + bbox[2]
+
+var overpassUrl = 'https://overpass-api.de/api/interpreter?data=[out:json];way["highway"](' + validBbox + ');out body;>;out skel qt;';
+
+console.log(overpassUrl)
+fetch(overpassUrl)
+  .then(response => response.json())
+  .then(data => {
+    var geojson = osmtogeojson(data);
+
+    // add the GeoJSON data to the Leaflet map
+    L.geoJSON(geojson).addTo(map);
+});
